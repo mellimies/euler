65f51671ec0efcaf570f1a0144acbb39af22fb94
Jaska <jaska@Jaakko-MacBook-Pro.local>
2017-09-09 13:23:36 +0300

    Euler problem 36 solution.

---
+++ p36.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# https://projecteuler.net/problem=36
+# Double-base palindromes
+
+# Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+
+from timeit import default_timer as timer
+
+t_start = timer()
+
+def is_palindromic(x):
+    return str(x) == str(x)[::-1]
+
+limit = 1000000
+
+nums = xrange(1, limit)
+nums = filter(is_palindromic, nums)
+nums = filter(lambda x: is_palindromic(bin(x)[2:]), nums)
+
+print nums, sum(nums)
+rv = sum(nums)
+print rv, timer() - t_start
