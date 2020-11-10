284e7e0dc4290ea942c4c9c3ef2636709929199a
Jaakko Puurunen <jaakko.puurunen@gmail.com>
2020-11-10 17:17:10 +0200

    Problem 39 with scala.
    ----------
    
    Congratulations, the answer you gave to problem 39 is correct.
    
    You are the 72257th person to have solved this problem.
    
    This problem had a difficulty rating of 5%. The highest difficulty rating you have solved so far is 30%.

---
+++ scala/src/main/scala/euler/p039.scala
@@ -0,0 +1,41 @@
+package main.scala.euler
+
+import euler.EulerTools.time
+
+object p039 extends App {
+
+  /**
+    * Integer right triangles
+    *
+    * If p is the perimeter of a right angle triangle with integral length sides, {a,b,c},
+    * there are exactly three solutions for p = 120.
+    *
+    * {20,48,52}, {24,45,51}, {30,40,50}
+    *
+    * For which value of p ≤ 1000, is the number of solutions maximised?
+    */
+
+  time {
+
+    // p = a + b + c, a**2 + b**2 = c**2
+    def isValidTriangle(p: Int, a: Int, b: Int): Boolean = p * p - 2 * p * (a + b) + 2 * a * b == 0
+
+    val pLimit = 1000
+
+    val trianglesWithP = for {
+      p <- 1 to pLimit
+      a <- 1 to p // surely can be optimized but brute force works too, runtime a couple of seconds
+      b <- a to p // a is smallest side
+      if isValidTriangle(p, a, b)
+    } yield p -> (a, b)
+
+    val pWithNumSolutions = trianglesWithP.groupBy(_._1).mapValues(_.size) // p -> count(solutions)
+    val mostSolutions = pWithNumSolutions.fold((0, 0)) {
+      (acc, candidate) => if (candidate._2 > acc._2) (candidate._1, candidate._2) else acc
+    }
+    val ans = mostSolutions._1
+    println(s"P=${mostSolutions._1} has most solutions with count: ${mostSolutions._2}")
+    println(s"Answer: $ans")
+    assert(ans == 840, "Expected 840")
+  }
+}
